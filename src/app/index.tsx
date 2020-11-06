26a83c01395c7031b0f0d8ce0d47f5d226c3fd0a
ElCholoGamer <josedanielgrayson@gmail.com>
2020-11-05 22:00:32 -0500

    hmm

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,13 +4,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-if (process.env.NODE_ENV === 'production') {
-	// Disable logs on production
-	console.log = function () {};
-	console.warn = function () {};
-	console.error = function () {};
-}
-
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
