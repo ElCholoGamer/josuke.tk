706d5f58506dc738e98a704342d6be99242dc6ee
ElCholoGamer <josedanielgrayson@gmail.com>
2020-11-05 21:47:32 -0500

    more debug

--- src/app/pages/GuildDashboard/index.tsx
+++ src/app/pages/GuildDashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import LinkedButton from 'components/LinkedButton/index';
-import SaveFooter from './SaveFooter';
-import Setting from 'components/Setting';
-import { compareObjects, Config, User } from '../../utils';
+// import LinkedButton from 'components/LinkedButton/index';
+// import SaveFooter from './SaveFooter';
+// import Setting from 'components/Setting';
+import { Config, User } from '../../utils';
 import './GuildDashboard.scss';
 
 interface Props {
@@ -12,12 +12,13 @@ interface Props {
 
 const GuildDashboard: React.FC<Props> = ({ user }) => {
 	const [config, setConfig] = React.useState<Config | null>(null);
-	const [prevConfig, setPrevConfig] = React.useState<Config | null>(null);
-	const [saveButtons, setSaveButtons] = React.useState(true);
+	const [, setPrevConfig] = React.useState<Config | null>(null);
+	// const [saveButtons, setSaveButtons] = React.useState(true);
 
 	const { guildId } = useParams<{ guildId: string }>();
 
 	React.useEffect(() => {
+		if (!user) return;
 		let timeout: number | null = null;
 
 		const fetchConfig = async (accessToken: string) => {
@@ -42,7 +43,7 @@ const GuildDashboard: React.FC<Props> = ({ user }) => {
 			}
 		};
 
-		if (!config && user) fetchConfig(user.accessToken).catch(console.error);
+		if (!config) fetchConfig(user.accessToken).catch(console.error);
 
 		return () => {
 			if (timeout) clearTimeout(timeout);
@@ -56,109 +57,109 @@ const GuildDashboard: React.FC<Props> = ({ user }) => {
 
 	document.title = `Dashboard | ${config.guildName}`;
 
-	const prefixLimiter = (str: string) =>
-		str.trimStart().replace(/\s+$/, ' ').toLowerCase().substr(0, 15);
+	// const prefixLimiter = (str: string) =>
+	// 	str.trimStart().replace(/\s+$/, ' ').toLowerCase().substr(0, 15);
 
-	const handleClick = () => {
-		// Check if string fields are okay
-		const { prefix, level_message, levels, send_level } = config;
-		if (!prefix.length) {
-			alert('Cannot have an empty prefix!');
-			return;
-		} else if (!level_message.length && levels && send_level) {
-			alert('Cannot have an empty level-up message!');
-			return;
-		}
+	// const handleClick = () => {
+	// 	// Check if string fields are okay
+	// 	const { prefix, level_message, levels, send_level } = config;
+	// 	if (!prefix.length) {
+	// 		alert('Cannot have an empty prefix!');
+	// 		return;
+	// 	} else if (!level_message.length && levels && send_level) {
+	// 		alert('Cannot have an empty level-up message!');
+	// 		return;
+	// 	}
 
-		// Disable buttons
-		setSaveButtons(false);
+	// 	// Disable buttons
+	// 	setSaveButtons(false);
 
-		// Update config
-		fetch(
-			`/config/${guildId}?authorization=${encodeURIComponent(
-				`Bearer ${user.accessToken}`
-			)}`,
-			{
-				method: 'PUT',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(config),
-			}
-		)
-			.then(res => {
-				if (res.status === 200) {
-					setPrevConfig(config);
-				} else console.log('Failed to save config:', res);
-			})
-			.catch(console.error)
-			.finally(() => setSaveButtons(true));
-	};
+	// 	// Update config
+	// 	fetch(
+	// 		`/config/${guildId}?authorization=${encodeURIComponent(
+	// 			`Bearer ${user.accessToken}`
+	// 		)}`,
+	// 		{
+	// 			method: 'PUT',
+	// 			headers: { 'Content-Type': 'application/json' },
+	// 			body: JSON.stringify(config),
+	// 		}
+	// 	)
+	// 		.then(res => {
+	// 			if (res.status === 200) {
+	// 				setPrevConfig(config);
+	// 			} else console.log('Failed to save config:', res);
+	// 		})
+	// 		.catch(console.error)
+	// 		.finally(() => setSaveButtons(true));
+	// };
 
-	return (
-		<h1>Something lol</h1>
-		// <main className="dashboard-main">
-		// 	<LinkedButton
-		// 		to="/dashboard"
-		// 		style={{
-		// 			width: '90px',
-		// 			height: '40px',
-		// 			fontSize: '17px',
-		// 			margin: '30px 0 0 30px',
-		// 		}}>
-		// 		Go Back
-		// 	</LinkedButton>
-		// 	<div className="dashboard-content">
-		// 		<h1>
-		// 			Dashboard for{' '}
-		// 			<span style={{ fontWeight: 700 }}>{config.guildName}</span>
-		// 		</h1>
-		// 		<Setting
-		// 			name="prefix"
-		// 			config={config}
-		// 			setConfig={setConfig}
-		// 			type="text"
-		// 			stringFormat={prefixLimiter}>
-		// 			Bot prefix:
-		// 		</Setting>
-		// 		<Setting
-		// 			name="snipe"
-		// 			config={config}
-		// 			setConfig={setConfig}
-		// 			type="toggle">
-		// 			Message sniping:
-		// 		</Setting>
+	return <h1>HMMM</h1>;
+	// return (
+	// 	<main className="dashboard-main">
+	// 		<LinkedButton
+	// 			to="/dashboard"
+	// 			style={{
+	// 				width: '90px',
+	// 				height: '40px',
+	// 				fontSize: '17px',
+	// 				margin: '30px 0 0 30px',
+	// 			}}>
+	// 			Go Back
+	// 		</LinkedButton>
+	// 		<div className="dashboard-content">
+	// 			<h1>
+	// 				Dashboard for{' '}
+	// 				<span style={{ fontWeight: 700 }}>{config.guildName}</span>
+	// 			</h1>
+	// 			<Setting
+	// 				name="prefix"
+	// 				config={config}
+	// 				setConfig={setConfig}
+	// 				type="text"
+	// 				stringFormat={prefixLimiter}>
+	// 				Bot prefix:
+	// 			</Setting>
+	// 			<Setting
+	// 				name="snipe"
+	// 				config={config}
+	// 				setConfig={setConfig}
+	// 				type="toggle">
+	// 				Message sniping:
+	// 			</Setting>
 
-		// 		<h2 className="config-subtitle">Leveling:</h2>
-		// 		<Setting
-		// 			name="levels"
-		// 			config={config}
-		// 			setConfig={setConfig}
-		// 			type="toggle">
-		// 			Enabled:
-		// 		</Setting>
-		// 		<Setting
-		// 			name="send_level"
-		// 			config={config}
-		// 			setConfig={setConfig}
-		// 			type="toggle">
-		// 			Send level-up message:
-		// 		</Setting>
-		// 		<Setting
-		// 			name="level_message"
-		// 			config={config}
-		// 			setConfig={setConfig}
-		// 			maxLength={200}
-		// 			type="textarea">
-		// 			Level-up message:
-		// 		</Setting>
-		// 		<SaveFooter
-		// 			enabled={!compareObjects(config, prevConfig)}
-		// 			buttonsEnabled={saveButtons}
-		// 			onSave={handleClick}
-		// 			onReset={() => setConfig(prevConfig)}
-		// 		/>
-		// 	</div>
-		// </main>
-	);
+	// 			<h2 className="config-subtitle">Leveling:</h2>
+	// 			<Setting
+	// 				name="levels"
+	// 				config={config}
+	// 				setConfig={setConfig}
+	// 				type="toggle">
+	// 				Enabled:
+	// 			</Setting>
+	// 			<Setting
+	// 				name="send_level"
+	// 				config={config}
+	// 				setConfig={setConfig}
+	// 				type="toggle">
+	// 				Send level-up message:
+	// 			</Setting>
+	// 			<Setting
+	// 				name="level_message"
+	// 				config={config}
+	// 				setConfig={setConfig}
+	// 				maxLength={200}
+	// 				type="textarea">
+	// 				Level-up message:
+	// 			</Setting>
+	// 			<SaveFooter
+	// 				enabled={!compareObjects(config, prevConfig)}
+	// 				buttonsEnabled={saveButtons}
+	// 				onSave={handleClick}
+	// 				onReset={() => setConfig(prevConfig)}
+	// 			/>
+	// 		</div>
+	// 	</main>
+	// );
 };
 
 export default GuildDashboard;
