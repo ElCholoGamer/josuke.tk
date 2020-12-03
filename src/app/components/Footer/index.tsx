0101bf8a7be51cc5145edd0e0e7602f894b09782
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-03 18:09:34 -0500

    Oh I forgot the footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer: React.FC = () => (
-	<footer>&copy; Copyright {new Date().getFullYear()}, ElCholoGamer</footer>
+	<footer className="text-light">&copy; Copyright {new Date().getFullYear()}, ElCholoGamer</footer>
 );
 
 export default Footer;
