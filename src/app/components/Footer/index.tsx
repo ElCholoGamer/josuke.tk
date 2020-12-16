d92054058e45a3dea1c44969a8e346c0af7914b1
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-15 20:08:31 -0500

    fix footer text

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer: React.FC = () => (
-	<footer className="text-light">&copy; Copyright {new Date().getFullYear()}, ElCholoGamer</footer>
+	<footer className="text-light d-flex">
+		&copy; Copyright {new Date().getFullYear()}, ElCholoGamer
+	</footer>
 );
 
 export default Footer;
