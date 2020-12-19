579be44f8dbaba97ae9525e2c6f51742cf385e70
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-18 20:03:10 -0500

    add pwa capabilities

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,12 +1,13 @@
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const common = require('./webpack.common');
 
 const config = merge(common, {
 	mode: 'production',
 	plugins: [
 		new webpack.optimize.SplitChunksPlugin(),
-		new webpack.optimize.AggressiveMergingPlugin(),
+		new WorkboxPlugin.GenerateSW(),
 	],
 	optimization: {
 		splitChunks: {
