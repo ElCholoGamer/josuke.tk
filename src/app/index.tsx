fa22f950521b1f2a5fddb601bbcb1b091bf2873f
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-18 21:42:34 -0500

    hope this works too

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,3 +10,11 @@ render(
 	</BrowserRouter>,
 	document.getElementById('root')
 );
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('/service-worker.js')
+			.then(reg => console.log(`Registration succeeded. Scope is ${reg.scope}`))
+			.catch(err => console.log('SW registration failed: ', err));
+	});
+}
