0a41107e6c7d8dc372688b7d3a32da4f9d498518
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-18 20:58:35 -0500

    maybe this works

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,13 +1,13 @@
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 const common = require('./webpack.common');
 
 const config = merge(common, {
 	mode: 'production',
 	plugins: [
 		new webpack.optimize.SplitChunksPlugin(),
-		new WorkboxPlugin.GenerateSW(),
+		new GenerateSW({ clientsClaim: true, skipWaiting: true }),
 	],
 	optimization: {
 		splitChunks: {
